d6670e22b5ba3a3961b51c455567b43713c08b21
21000660 <21000660@alumnos.utleon.edu.mx>
2024-10-26 10:32:14 -0600

    Api Flask

---
+++ env/src/config.py
@@ -0,0 +1,10 @@
+class DevelopmentConfig():
+    DEBUG = True
+    MYSQL_USER='root'
+    MYSQL_HOST='localhost'
+    MYSQL_PASSWORD=''
+    MYSQL_DB='api_utl'
+
+config={
+    'development':DevelopmentConfig
+}
